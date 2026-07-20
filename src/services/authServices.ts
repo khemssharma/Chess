0522eb8b5c8b4b209cf99938e554324703348668
Ayush Sharma <ayushsharma3310@gmail.com>
2026-07-20 12:26:34 +0530

    Update import path for prisma to include .js extension

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../lib/prisma';
+import { prisma } from '../lib/prisma.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET!;
